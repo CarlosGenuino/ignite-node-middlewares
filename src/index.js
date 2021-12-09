cad354969bd77105da2f64173816e581695909ce
Carlos Genuino <carloxsgenuino3@live.com>
2021-12-09 18:41:30 -0300

    resolvendo middleware

--- src/index.js
+++ src/index.js
@@ -11,18 +11,60 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers
+  const user = users.find(u => u.username === username)
+  if(!user){
+    return response.status(404).json({error: 'User Already Exists'})
+  }
+  request.user = user
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const user = request.user
+  if (!user.pro && user.todos.length >= 10){
+    return response.status(403).json({error: 'You just can have 10 itens in basic plan'})
+  }
+  console.log(user.todos.length)
+  next()
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+  const user = users.find(u => u.username === username)
+  
+  const validId = validate(id)
+  if(!validId){
+    return response.status(400).json({error: 'Invalid uuid'})
+  }
+
+  if(!user){
+    return response.status(404).json({error: 'user not found'})
+  }
+    
+  const todo = user.todos.find(t => t.id === id)
+  if(!todo){
+    return response.status(404).json({error: 'todo not found'})
+  }
+  request.user = user
+  request.todo = todo
+
+  next()
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params
+  const user = users.find(u => u.id === id)
+  if(!user) {
+    return response.status(404).json({error: 'user not found'})
+  }
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
@@ -43,7 +85,7 @@ app.post('/users', (request, response) => {
   };
 
   users.push(user);
-
+ // console.log(user)
   return response.status(201).json(user);
 });
 
